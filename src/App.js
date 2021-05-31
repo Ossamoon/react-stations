718a937b31f6f14c9fe55a6f44dfe533428c96c8
Ossamoon <osamu950623@gmail.com>
2021-05-31 19:56:32 +0900

    try station 6

--- src/App.js
+++ src/App.js
@@ -11,25 +11,41 @@ export const App = () => {
   const [dogUrl, setDogUrl] = useState(
     'https://images.dog.ceo/breeds/mastiff-tibetan/n02108551_175.jpg',
   )
+  const [error, setError] = useState(null)
+  const [isLoading, setIsLoading] = useState(true)
 
-  return (
-    <div className="App">
-      <header>
-        <h1>Dogアプリ</h1>
-      </header>
-      <body>
-        <p>犬の画像を表示するサイトです</p>
-        <img src={dogUrl} alt="[Dog Image]" />
-        <button
-          onClick={() =>
-            setDogUrl(
-              'https://images.dog.ceo/breeds/pembroke/n02113023_12192.jpg',
-            )
-          }
-        >
-          Change Image
-        </button>
-      </body>
-    </div>
-  )
+  const Reload = () => {
+    setIsLoading(false)
+    fetch('https://dog.ceo/api/breeds/image/random')
+      .then(res => res.json())
+      .then(
+        result => {
+          setIsLoading(true)
+          setDogUrl(result.message)
+        },
+        err => {
+          setIsLoading(true)
+          setError(err)
+        },
+      )
+  }
+
+  if (error) {
+    return <div>Error: {error.message}</div>
+  } else if (!isLoading) {
+    return <div>...Loading</div>
+  } else {
+    return (
+      <div className="App">
+        <header>
+          <h1>Dogアプリ</h1>
+        </header>
+        <body>
+          <p>犬の画像を表示するサイトです</p>
+          <img src={dogUrl} alt="[Dog Image]" />
+          <button onClick={() => Reload()}>Change Image</button>
+        </body>
+      </div>
+    )
+  }
 }
