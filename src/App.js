bd3436fd60fad3f47482e12242c9e1215e2d6912
Ossamoon <osamu950623@gmail.com>
2021-05-31 20:28:58 +0900

    try station 6

--- src/App.js
+++ src/App.js
@@ -11,29 +11,29 @@ export const App = () => {
   const [dogUrl, setDogUrl] = useState(
     'https://images.dog.ceo/breeds/mastiff-tibetan/n02108551_175.jpg',
   )
-  // const [error, setError] = useState(null)
+  const [error, setError] = useState(null)
   const [isLoading, setIsLoading] = useState(true)
 
   const Reload = () => {
     setIsLoading(false)
     fetch('https://dog.ceo/api/breeds/image/random')
-      .then(res => res.json())
+      // .then(res => res.json())
       .then(
         result => {
           setIsLoading(true)
           setDogUrl(result.message)
         },
-        // err => {
-        //   setIsLoading(true)
-        //   setError(err)
-        // },
+        err => {
+          setIsLoading(true)
+          setError(err)
+        },
       )
   }
 
   if (!isLoading) {
     return <div>...Loading</div>
-    // } else if (error) {
-    //   return <div>Error: {error.message}</div>
+  } else if (error) {
+    return <div>Error: {error.message}</div>
   } else {
     return (
       <div className="App">
