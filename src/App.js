cecdab85ea4e99f0a2e6bb6444437f4617728df5
Ossamoon <osamu950623@gmail.com>
2021-05-31 17:08:58 +0900

    try station 2

--- src/App.js
+++ src/App.js
@@ -4,13 +4,15 @@ import * as React from 'react'
 import './App.css'
 
 /**
- * 
+ *
  * @type {React.FC}
  */
 export const App = () => {
   return (
-    <div>
-      <h2>Hello, world!</h2>
+    <div className="App">
+      <header>
+        <h1>Dogアプリ</h1>
+      </header>
     </div>
   )
 }
